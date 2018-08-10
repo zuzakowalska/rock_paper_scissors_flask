157dbca7c5cc59caaccf00b398668bbfd859c373
Zuzanna Kowalska <zuzanna.kowalska@brandnova.com>
2018-08-10 12:31:10 +0200

    use db in game

--- app/routes.py
+++ app/routes.py
@@ -17,8 +17,9 @@ def session_management():
 def index():
     form = PlayerName()
     if form.validate_on_submit():
-        player = GameData(player_name=form.player_name.data, player_score=0)
-        db.session.add(player)
+        new_player = GameData(
+            player_name=form.player_name.data, player_score=0)
+        db.session.add(new_player)
         db.session.commit()
         return redirect('/game')
     return render_template('index.html', title="Rock Paper Scissors", form=form)
@@ -26,10 +27,13 @@ def index():
 
 @app.route('/<move>')
 def moves_list(move):
+    player = GameData.query.filter_by(
+        player_name=GameData.player_name).order_by(GameData.id.desc()).first()
     single_game = Game(moves)
     cp = single_game.setup(move).cp_name
-    session['round_score'] = single_game.round_check()
-    round_score = session['round_score']
+    round_score = single_game.round_check()
+    player.player_score = player.player_score + round_score
+    db.session.commit()
     return render_template('moves_list.html', title="Rock Paper Scissors", move=move, cp=cp, round_score=round_score)
 
 
@@ -38,5 +42,16 @@ def game_round():
     player = GameData.query.filter_by(
         player_name=GameData.player_name).order_by(GameData.id.desc()).first()
     player_name = player.player_name
-    game_score = 0
-    return render_template('game.html', title="Rock Paper Scissors", moves=moves, game_score=game_score, ascii_art=ascii_art, player_name=player_name)
+    player_score = player.player_score
+    if player_score == 3:
+        return render_template('win.html', title="Rock Paper Scissors", player_name=player_name)
+    elif player_score == -3:
+        return render_template('lose.html', title="Rock Paper Scissors", player_name=player_name)
+    else:
+        return render_template('game.html', title="Rock Paper Scissors", moves=moves, player_score=player_score, ascii_art=ascii_art, player_name=player_name)
+
+
+@app.route('/new-game')
+def new_game():
+    session.clear()
+    return redirect('/')
